29c124919f00ba89938563df1abebdbbc2181fff
Tejas Shahpuri <32913904+tshahpuri@users.noreply.github.com>
2018-02-16 16:11:56 -0500

    Update direct_keys.py

--- direct_keys.py
+++ direct_keys.py
@@ -1,5 +1,4 @@
 # direct inputs
-# source to this solution and code:
 # http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
 # http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
 
