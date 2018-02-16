87914d9f80f184c98b73d897522e1a57cf0aa948
Tejas Shahpuri <32913904+tshahpuri@users.noreply.github.com>
2018-02-16 16:10:41 -0500

    Update create_lstm_training_data.py

--- create_lstm_training_data.py
+++ create_lstm_training_data.py
@@ -1,11 +1,6 @@
 # coding: utf-8
 
-# # Object Detection Demo
-# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/installation.md) before you start.
-
 # # Imports
-
-# In[70]:
 
 
 import os
